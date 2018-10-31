5146ec43c8d46858bd51fade3325286fd228d19b
sadmiraga <sadmirvela@gmail.com>
2018-10-31 14:04:40 +0100

    login/register form

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view ('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
