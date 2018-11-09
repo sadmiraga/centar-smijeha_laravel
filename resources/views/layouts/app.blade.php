cc404a3719d0f06e2df3351d0ef8f46bd1a6c963
sadmiraga <sadmirvela@gmail.com>
2018-11-09 23:35:55 +0100

    fixed nesto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                            
 
                         @else
-                    <li><a href="{{ url('mojprofil', ['id'=> {{Auth::id()}}]) }}">Moj Profil</a></li>
+                        <li><a href="/mojprofil/1">Moj Profil</a></li>
                         <li><a href="/kategorije">Kategorije</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
