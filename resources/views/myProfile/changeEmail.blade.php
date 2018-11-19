001445487c522ac3d7a163f2d5029c423f03b8c3
sadmiraga <sadmirvela@gmail.com>
2018-11-19 11:09:42 +0100

    promjeni Username & Email Design fix (done)

--- resources/views/myProfile/changeEmail.blade.php
+++ resources/views/myProfile/changeEmail.blade.php
@@ -6,6 +6,11 @@
     <?php
         $stariEmail = Auth::user()->email;
     ?>
+
+    <!-- NASLOV STRANICE -->
+    <div class="well" id="usredini">
+        <h1> Promjeni Email </h1>
+    </div>
     
     <!-- ERRORI -->
     @if(count($errors)>0)
@@ -16,14 +21,15 @@
         @endforeach
     @endif
 
+
+    <div class="well" id="usredini">
     <!-- FORMA -->
-    {!! Form::open(['action'=>'profileController@changeEmailSubmit','method'=>'POST'])!!}
-        {{Form::text('newEmail',"$stariEmail",['class'=>'form-control'])}}
-
-        <div style="text-align:center;">
-            {{Form::submit('spremi',['class'=>'btn btn-primary'])}}
-        </div>
-    
+        {!! Form::open(['action'=>'profileController@changeEmailSubmit','method'=>'POST'])!!}
+            {{Form::text('newEmail',"$stariEmail",['class'=>'form-control'])}}
+            <br>
+            <div style="text-align:center;">
+                {{Form::submit('spremi',['class'=>'btn btn-primary'])}}
+            </div>
         {!! FOrm::close()!!}
-
+    </div>
 @endsection
--- resources/views/myProfile/changeUsername.blade.php
+++ resources/views/myProfile/changeUsername.blade.php
@@ -7,6 +7,11 @@
         $staroIme = Auth::user()->name;
     ?>
 
+    <!-- NASLOV STRANICE -->
+    <div class="well" id="usredini">
+        <h1> Promjeni korisničko ime </h1>
+    </div>
+
     <!-- ERRORI -->
     @if(count($errors)>0)
         @foreach($errors->all() as $error)
@@ -15,17 +20,19 @@
             </div>
         @endforeach
     @endif
-
-    <!-- FORMA -->
-    {!! Form::open(['action'=>'profileController@changeUsernameSubmit', 'method'=>'POST']) !!}
     
-    <!-- text emaila-->
-    {{ Form::text("newUsername","$staroIme",['class'=>'form-control'])}}
-    
-    <!-- submit button -->
-    <div style="text-align:center;">
-    {{Form::submit('Spremi',['class'=>'btn btn-primary'])}}
-    {!! Form::close()!!}
+    <div class="well" id="usredini">
+        <!-- FORMA -->
+        {!! Form::open(['action'=>'profileController@changeUsernameSubmit', 'method'=>'POST']) !!}
+        
+        <!-- text emaila-->
+        {{ Form::text("newUsername","$staroIme",['class'=>'form-control'])}}
+        <br>
+        <!-- submit button -->
+        <div style="text-align:center;">
+        {{Form::submit('Spremi',['class'=>'btn btn-primary'])}}
+        {!! Form::close()!!}
+        </div>
     </div>
 
 
